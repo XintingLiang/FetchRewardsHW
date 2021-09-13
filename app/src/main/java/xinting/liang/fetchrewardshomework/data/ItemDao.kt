d149365ca796c4444d2b631e7879759d1a2167c3
Xinting Liang <lily800905@gmail.com>
2021-09-13 13:55:23 -0700

    Filtered data based on ListID

--- app/src/main/java/xinting/liang/fetchrewardshomework/data/ItemDao.kt
+++ app/src/main/java/xinting/liang/fetchrewardshomework/data/ItemDao.kt
@@ -7,8 +7,8 @@ import retrofit2.http.DELETE
 @Dao
 interface ItemDao {
 
-//    @Query("SELECT * FROM item WHERE listId = listID")
-//    fun getItemBasedOnListId(listID: Int): LiveData<List<ItemEntity>>
+    @Query("SELECT * FROM item WHERE listId = :listID AND name IS NOT NULL AND nameNumber > 0 ORDER BY nameNumber ASC")
+    fun getItemBasedOnListId(listID: Int): LiveData<List<ItemEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(items: List<ItemEntity>)
@@ -17,5 +17,5 @@ interface ItemDao {
     fun delete(item: ItemEntity)
 
     @Query("SELECT * FROM item WHERE name IS NOT NULL AND nameNumber > 0 ORDER BY listId ASC, nameNumber ASC")
-    fun filterNameOrderByNameAndId():LiveData<List<ItemEntity>>
+    fun filterNameOrderByNameAndId(): LiveData<List<ItemEntity>>
 }
